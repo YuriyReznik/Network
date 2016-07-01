b31bbe21c120ed9dd79807d7b0fc063df895e3b3
YuriyReznik <yurij.reznik@gmail.com>
2016-07-01 17:01:21 +0300

    add accordeon for comments inside each report

--- app/assets/javascripts/profile.js
+++ app/assets/javascripts/profile.js
@@ -1,4 +1,25 @@
-// function Myfunction(){
-//     document.getElementById('test').innerHTML =
-//         '<a href="#"> Hello </a>';
-// }
+$(document).ready(function () {
+    $('.accordeon_trigger').on('click', function (e) {
+        e.preventDefault();
+
+        var $this = $(this),
+            item = $this.closest('.accordeon_item'),
+            list = $this.closest('.accordeon_list'),
+            items = list.find('.accordeon_item'),
+            content = item.find('.accordeon_inner'),
+            otherContent = list.find('.accordeon_inner'),
+            duration = 300;
+
+        if(!item.hasClass('active')) {
+            items.removeClass('active');
+            item.addClass('active');
+
+            otherContent.stop(true, true).slideUp(duration);
+            content.stop(true, true).slideDown(duration);
+        } else {
+            content.stop(true, true).slideUp(duration);
+            item.removeClass('active');
+        }
+    });
+
+});
